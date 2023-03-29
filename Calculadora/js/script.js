5bf9de7110aa1135f774001b6a226ece8a902b7d
DanilloF <124094043+DanilloFelipe@users.noreply.github.com>
2023-03-29 09:46:46 -0300

    FinalCalc

--- Calculadora/js/script.js
+++ Calculadora/js/script.js
@@ -19,14 +19,109 @@ class Calculator {
         this.updateScreen()
     }
 
-    // Process all calculator operation
+    // Process all calculator operations
     processOperation(operation) {
-        console.log(operation)
+        //Check if current is empty
+        if (this.currentOperationText.innerText === "" && operation !== "C") {
+            // Change operation
+            if (this.previousOperationText.innerText !== "") {
+                this.changeOperation(operation)
+            }
+            return
+        }
+
+        // Get current and previous value
+        let operationValue
+        const previous = +this.previousOperationText.innerText.split(" ")[0]
+        const current = +this.currentOperationText.innerText
+
+        switch(operation) {
+            case "+":
+                operationValue = previous + current
+                this.updateScreen(operationValue, operation, current, previous)
+                break
+            case "-":
+                operationValue = previous - current
+                this.updateScreen(operationValue, operation, current, previous)
+                break
+            case "/":
+                operationValue = previous / current
+                this.updateScreen(operationValue, operation, current, previous)
+                break
+            case "*":
+                operationValue = previous * current
+                this.updateScreen(operationValue, operation, current, previous)
+                break
+            case "DEL":
+                this.processDelOperator()
+                break
+            case "CE":
+                this.processClearCurrentOperation()
+                break
+            case "C":
+                this.processClearOperation()
+                break
+            case "=":
+                this.processEqualOperator()
+                break
+            default:
+                return
+        }
     }
 
     // Change values of the calculator screen
-    updateScreen() {
-        this.currentOperationText.innerText += this.currentOperation
+    updateScreen(
+        operationValue = null, 
+        operation = null, 
+        current = null,
+        previous = null
+        ) {
+        console.log(operationValue, operation, current, previous)
+        if (operationValue === null) {
+            this.currentOperationText.innerText += this.currentOperation
+        } else {
+            // Check if value is zero, if it is just add current value
+            if (previous == 0) {
+                operationValue = current
+            }
+            // Add current value to previous
+            this.previousOperationText.innerText = `${operationValue} ${operation}`
+            this.currentOperationText.innerText = ""
+        }
+    }
+
+    // Change math Operation
+    changeOperation(operation) {
+        const mathOperations = ["*", "/", "+", "-"]
+
+        if(!mathOperations.includes(operation)) {
+            return
+        }
+
+        this.previousOperationText.innerText = this.previousOperationText.innerText.slice(0, -1) + operation
+    }
+
+    // Delete the last digit
+    processDelOperator() {
+        this.currentOperationText.innerText = 
+            this.currentOperationText.innerText.slice(0, -1)
+    }
+
+    // Clear current operation
+    processClearCurrentOperation() {
+        this.currentOperationText.innerText = ""
+    }
+
+    // Clear All Operations
+    processClearOperation() {
+        this.currentOperationText.innerText=""
+        this.previousOperationText.innerText=""
+    }
+
+    // Equal Operation
+    processEqualOperator() {
+        const operation = previousOperationText.innerText.split(" ")[1]
+        this.processOperation(operation)
     }
 }
 
